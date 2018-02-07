22503b0d643e9cb049cc082537d2a2cb2d4acb03
TestGetVideoFristViewImage <924686691@qq.com>
2018-02-07 16:27:07 +0800

    注释

--- TestGetVideoFristViewImage/ViewController.swift
+++ TestGetVideoFristViewImage/ViewController.swift
@@ -7,17 +7,30 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let thumb = UIImageView.init(frame: CGRect.init(x: 50, y: 200, width: 300, height: 200))
+        self.view.addSubview(thumb)
+        let asset = AVURLAsset.init(url: URL.init(string: "http://gslb.miaopai.com/stream/1UKfVpOmazRYEb4fVejwhgpX~3uIxmHBV~8VCQ__.mp4")!, options: nil)
+        let gen = AVAssetImageGenerator.init(asset: asset)
+        gen.appliesPreferredTrackTransform = true
+        let time = CMTimeMakeWithSeconds(0.0, 1)
+        var actualTime : CMTime = CMTimeMakeWithSeconds(0, 0)
+        do {
+            let image = try gen.copyCGImage(at: time, actualTime: &actualTime)
+            thumb.image = UIImage.init(cgImage: image)
+        } catch  {
+            print("错误")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
